cb1304cef966c99fdbed330b46bfeb59a980c68a
ElhakimDev99 <bagusajieiswara30@gmail.com>
2024-11-10 22:50:39 +0700

    fix: accordion

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { NgxPrimerAccordionRootComponent } from '@ngx-primer-accordion';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, NgxPrimerAccordionRootComponent],
+  imports: [RouterModule, NgxPrimerAccordionRootComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
