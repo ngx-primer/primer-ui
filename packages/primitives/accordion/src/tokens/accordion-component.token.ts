860f46d76015587e43f4e98d77f9238f7187e2ac
ATS - Elhakim Cool <bagusajieiswara30@gmail.com>
2024-12-21 04:21:36 +0700

    fix: add docs

--- packages/primitives/accordion/src/tokens/accordion-component.token.ts
+++ packages/primitives/accordion/src/tokens/accordion-component.token.ts
@@ -7,18 +7,49 @@ import {
 
 import { InjectionToken } from '@angular/core';
 
+/**
+ * Injection token for the root component of the NgxPrimerAccordion.
+ * 
+ * This token is used to inject the root component of the accordion, which is
+ * of type `NgxPrimerAccordionRootComponent<unknown>`.
+ * 
+ * @constant
+ * @type {InjectionToken<NgxPrimerAccordionRootComponent<unknown>>}
+ */
 export const NgxPrimerAccordionRootComponentToken = new InjectionToken<
   NgxPrimerAccordionRootComponent<unknown>
 >('NgxPrimerAccordionRootComponentToken');
 
+/**
+ * Injection token for the NgxPrimerAccordionItemComponent.
+ * 
+ * This token is used to inject the NgxPrimerAccordionItemComponent
+ * into other components or services.
+ * 
+ * @type {InjectionToken<NgxPrimerAccordionItemComponent<unknown>>}
+ */
 export const NgxPrimerAccordionItemComponentToken = new InjectionToken<
   NgxPrimerAccordionItemComponent<unknown>
 >('NgxPrimerAccordionItemComponentToken');
 
+/**
+ * Injection token for the NgxPrimerAccordionTriggerComponent.
+ * This token is used to inject the NgxPrimerAccordionTriggerComponent
+ * into other components or services.
+ *
+ * @type {InjectionToken<NgxPrimerAccordionTriggerComponent<unknown>>}
+ */
 export const NgxPrimerAccordionTriggerComponentToken = new InjectionToken<
   NgxPrimerAccordionTriggerComponent<unknown>
 >('NgxPrimerAccordionTriggerComponentToken');
 
+/**
+ * Injection token for the NgxPrimerAccordionContentComponent.
+ * This token is used to inject the NgxPrimerAccordionContentComponent
+ * into other components or services.
+ *
+ * @type {InjectionToken<NgxPrimerAccordionContentComponent<unknown>>}
+ */
 export const NgxPrimerAccordionContentComponentToken = new InjectionToken<
   NgxPrimerAccordionContentComponent<unknown>
 >('NgxPrimerAccordionContentComponentToken');
